9ae6db304a97bfa6d2fca18c72345412e9ee3a21
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-06-08 18:58:29 +0200

    finish v1

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { render } from 'react-dom'
+import './styles/index.css'
+import { Listings } from './sections'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const client = new ApolloClient({
+  uri: '/api',
+  cache: new InMemoryCache(),
+})
+
+render(
+  <ApolloProvider client={client}>
+    <Listings title="TinyHouse Listings" />
+  </ApolloProvider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
---
+++ src/sections/Listings/index.tsx
@@ -0,0 +1,111 @@
+import { gql, useMutation, useQuery } from '@apollo/client'
+import { Button, List, Spin, Avatar, Alert } from 'antd'
+import './styles/Listings.css'
+import { Listings as ListingsData } from './__generated__/Listings'
+import {
+  DeleteListing as DeleteListingData,
+  DeleteListingVariables,
+} from './__generated__/DeleteListing'
+import { ListingsSkeleton } from './components'
+
+const LISTINGS = gql`
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`
+
+const DELETE_LISTING = gql`
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`
+
+interface Props {
+  title: string
+}
+
+export const Listings = ({ title }: Props) => {
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS)
+  const [
+    deleteListing,
+    { loading: deleteListingLoading, error: deleteListingError },
+  ] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING)
+
+  const handleDeleteListing = async (id: string) => {
+    await deleteListing({ variables: { id } })
+    await refetch()
+  }
+
+  const listings = data?.listings
+
+  const listingsList = listings ? (
+    <List
+      itemLayout="horizontal"
+      dataSource={listings}
+      renderItem={(listing) => (
+        <List.Item
+          actions={[
+            <Button
+              type="primary"
+              onClick={() => handleDeleteListing(listing.id)}
+            >
+              Delete
+            </Button>,
+          ]}
+        >
+          <List.Item.Meta
+            title={listing.title}
+            description={listing.address}
+            avatar={<Avatar src={listing.image} shape="square" size={48} />}
+          />
+        </List.Item>
+      )}
+    />
+  ) : null
+
+  if (loading) {
+    return (
+      <div className="listings">
+        <ListingsSkeleton title={title} />
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className="listings">
+        <ListingsSkeleton title={title} error />
+      </div>
+    )
+  }
+
+  const deleteListingErrorAlert = deleteListingError ? (
+    <Alert
+      type="error"
+      message="Uh oh! Something went wrong :(. Please try again later."
+      className="listings__alert"
+    />
+  ) : null
+
+  return (
+    <div className="listings">
+      {deleteListingErrorAlert}
+      <Spin spinning={deleteListingLoading}>
+        <h2>{title}</h2>
+        {listingsList}
+      </Spin>
+    </div>
+  )
+}
