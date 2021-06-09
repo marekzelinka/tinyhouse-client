6cfd08ea6df57e309c2baf5c385cb2377f8e3e32
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-06-09 09:27:28 +0200

    setup routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,32 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { render } from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './styles/index.css'
-import { Listings } from './sections'
+import { Home, Host, Listing, Listings, NotFound, User } from './sections'
 
 const client = new ApolloClient({
   uri: '/api',
   cache: new InMemoryCache(),
 })
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
 render(
   <ApolloProvider client={client}>
-    <Listings title="TinyHouse Listings" />
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 )
