8f7d96cad67c0e7fcabd5397c912978fc65dcdd5
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-06-10 14:23:30 +0200

    add initial listing page

--- src/sections/Listing/index.tsx
+++ src/sections/Listing/index.tsx
@@ -1,3 +1,65 @@
+import { useQuery } from '@apollo/client'
+import { Col, Layout, Row } from 'antd'
+import { useState } from 'react'
+import { useParams } from 'react-router'
+import { ErrorBanner, PageSkeleton } from '../../lib/components'
+import { LISTING } from '../../lib/graphql/queries'
+import {
+  Listing as ListingData,
+  ListingVariables,
+} from '../../lib/graphql/queries/Listing/__generated__/Listing'
+import { ListingDetails } from './components'
+
+const { Content } = Layout
+
+const PAGE_LIMIT = 4
+
 export const Listing = () => {
-  return <h2>Listing</h2>
+  const { id } = useParams<{ id: string }>()
+  const [bookingsPage, setBookingsPage] = useState(1)
+  const { data, loading, error } = useQuery<ListingData, ListingVariables>(
+    LISTING,
+    {
+      variables: {
+        id: id,
+        bookingsPage,
+        limit: PAGE_LIMIT,
+      },
+    }
+  )
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <PageSkeleton />
+      </Content>
+    )
+  }
+
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner description="This listing may not exist or we've encountered an error. Please try again soon." />
+        <PageSkeleton />
+      </Content>
+    )
+  }
+
+  const listing = data?.listing
+
+  const listingBookings = listing?.bookings
+
+  const listingDetailsElement = listing ? (
+    <ListingDetails listing={listing} />
+  ) : null
+
+  return (
+    <Content className="listings">
+      <Row gutter={24} justify="space-between">
+        <Col xs={24} lg={14}>
+          {listingDetailsElement}
+        </Col>
+      </Row>
+    </Content>
+  )
 }
