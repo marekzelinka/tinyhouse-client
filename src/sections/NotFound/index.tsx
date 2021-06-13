1d60c8184b97f764475f67da4254288c495e69ba
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-06-13 17:41:35 +0200

    add not found page

--- src/sections/NotFound/index.tsx
+++ src/sections/NotFound/index.tsx
@@ -1,3 +1,30 @@
+import { Empty, Layout, Typography } from 'antd'
+import { Link } from 'react-router-dom'
+
+const { Content } = Layout
+const { Text } = Typography
+
 export const NotFound = () => {
-  return <h2>NotFound</h2>
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <>
+            <Text className="not-found__description-title">
+              Uh oh! Something went wrong :(
+            </Text>
+            <Text className="not-found__description-subtitle">
+              The page you're looking for can't be found
+            </Text>
+          </>
+        }
+      />
+      <Link
+        to="/"
+        className="not-found__cta ant-bnt ant-btn-primary ant-btn-lg"
+      >
+        Go to Home
+      </Link>
+    </Content>
+  )
 }
